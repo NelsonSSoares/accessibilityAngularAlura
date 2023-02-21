4c62b9a4e3f4d391ff23b49f0e4ea567394f2bcf
Nelson <punkpunkada@gmail.com>
2023-02-21 11:45:04 -0300

    compoenente de botão, configurar tslib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { YesNoButtonGroupComponent } from './shared/components/yes-no-button-group/yes-no-button-group.component';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YesNoButtonGroupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+ }
