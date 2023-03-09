18dbd21340b89001168775a3a932d588eaaa5104
Nelson <punkpunkada@gmail.com>
2023-03-09 13:03:43 -0300

    Keyboard logic

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -16,22 +16,43 @@ export class KeyboardManagerDirective{
         switch(event.key){
             case 'ArrowUp':
                 console.log(this.items);
-                
+                this.moveFocus(ArrowDirection.RIGHT).focus();
                 console.log('up');
             break;
             
             case 'ArrowDown':
+                this.moveFocus(ArrowDirection.LEFT).focus();
                 console.log('Down');
             break;
 
             case 'ArrowLeft':
+                this.moveFocus(ArrowDirection.LEFT).focus();
                 console.log('Left');
             break;
 
             case 'ArrowRight':
+                this.moveFocus(ArrowDirection.RIGHT).focus();
                 console.log('Right');
             break;
             
         }
     }
+
+    public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective{
+        const items = this.items.toArray();
+        const currentSelectedIndex = items.findIndex(item => item.isFocused());
+        const targetElementFocus = items[currentSelectedIndex + direction];
+        
+        if(targetElementFocus){
+            return targetElementFocus;
+        }
+        return direction === ArrowDirection.LEFT ? items[items.length - 1] : items[0];
+    }
+
+}
+
+enum ArrowDirection{
+    LEFT = -1,
+    RIGHT = 1,
+
 }
